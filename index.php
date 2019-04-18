e87bd2df7f0ad4cdbd6fecbc61c8a56f10ca9936
thiagu <thiyagu.devops@gmail.com>
2019-04-18 13:36:50 +0800

    updated

--- index.php
+++ index.php
@@ -37,7 +37,7 @@ $conn->close(); ?>
     <meta name="author" content="">
     <link rel="icon" href="book.ico">
 
-    <title>DevOps Demo Application</title>
+    <title>DevOps Demo Application - updated</title>
 
     <!-- Bootstrap core CSS -->
     <link href="bootstrap.min.css" rel="stylesheet">
